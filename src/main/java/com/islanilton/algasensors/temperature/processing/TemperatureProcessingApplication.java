aba67ffadab4b2859feb6a005f55af04079a684c
Islanilton Rodrigues <islanilton.rodrigues@gmail.com>
2025-11-05 13:51:10 -0300

    create a main class

---
+++ src/main/java/com/islanilton/algasensors/temperature/processing/TemperatureProcessingApplication.java
@@ -0,0 +1,10 @@
+package com.islanilton.algasensors.temperature.processing;
+
+import org.springframework.boot.SpringApplication;
+
+public class TemperatureProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureProcessingApplication.class, args);
+    }
+}
